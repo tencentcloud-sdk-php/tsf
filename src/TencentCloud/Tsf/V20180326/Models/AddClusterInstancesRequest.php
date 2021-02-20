9c9501a3f26ca6cf01caea8e1af80e19f63d89b9
tencentcloudapi <tencentcloudapi@tenent.com>
2021-02-20 17:23:36 +0800

    release 3.0.334

--- src/TencentCloud/Tsf/V20180326/Models/AddClusterInstancesRequest.php
+++ src/TencentCloud/Tsf/V20180326/Models/AddClusterInstancesRequest.php
@@ -38,6 +38,12 @@ use TencentCloud\Common\AbstractModel;
  * @method void setInstanceImportMode(string $InstanceImportMode) 设置云主机导入方式，虚拟机集群必填，容器集群不填写此字段，R：重装TSF系统镜像，M：手动安装agent
  * @method string getOsCustomizeType() 获取镜像定制类型
  * @method void setOsCustomizeType(string $OsCustomizeType) 设置镜像定制类型
+ * @method array getFeatureIdList() 获取镜像特征ID列表
+ * @method void setFeatureIdList(array $FeatureIdList) 设置镜像特征ID列表
+ * @method InstanceAdvancedSettings getInstanceAdvancedSettings() 获取实例额外需要设置参数信息
+ * @method void setInstanceAdvancedSettings(InstanceAdvancedSettings $InstanceAdvancedSettings) 设置实例额外需要设置参数信息
+ * @method array getSecurityGroupIds() 获取部署组ID
+ * @method void setSecurityGroupIds(array $SecurityGroupIds) 设置部署组ID
  */
 class AddClusterInstancesRequest extends AbstractModel
 {
@@ -87,6 +93,21 @@ class AddClusterInstancesRequest extends AbstractModel
     public $OsCustomizeType;
 
     /**
+     * @var array 镜像特征ID列表
+     */
+    public $FeatureIdList;
+
+    /**
+     * @var InstanceAdvancedSettings 实例额外需要设置参数信息
+     */
+    public $InstanceAdvancedSettings;
+
+    /**
+     * @var array 部署组ID
+     */
+    public $SecurityGroupIds;
+
+    /**
      * @param string $ClusterId 集群ID
      * @param array $InstanceIdList 云主机ID列表
      * @param string $OsName 操作系统名称
@@ -96,6 +117,9 @@ class AddClusterInstancesRequest extends AbstractModel
      * @param string $SgId 安全组设置
      * @param string $InstanceImportMode 云主机导入方式，虚拟机集群必填，容器集群不填写此字段，R：重装TSF系统镜像，M：手动安装agent
      * @param string $OsCustomizeType 镜像定制类型
+     * @param array $FeatureIdList 镜像特征ID列表
+     * @param InstanceAdvancedSettings $InstanceAdvancedSettings 实例额外需要设置参数信息
+     * @param array $SecurityGroupIds 部署组ID
      */
     function __construct()
     {
@@ -144,6 +168,19 @@ class AddClusterInstancesRequest extends AbstractModel
 
         if (array_key_exists("OsCustomizeType",$param) and $param["OsCustomizeType"] !== null) {
             $this->OsCustomizeType = $param["OsCustomizeType"];
+        }
+
+        if (array_key_exists("FeatureIdList",$param) and $param["FeatureIdList"] !== null) {
+            $this->FeatureIdList = $param["FeatureIdList"];
+        }
+
+        if (array_key_exists("InstanceAdvancedSettings",$param) and $param["InstanceAdvancedSettings"] !== null) {
+            $this->InstanceAdvancedSettings = new InstanceAdvancedSettings();
+            $this->InstanceAdvancedSettings->deserialize($param["InstanceAdvancedSettings"]);
+        }
+
+        if (array_key_exists("SecurityGroupIds",$param) and $param["SecurityGroupIds"] !== null) {
+            $this->SecurityGroupIds = $param["SecurityGroupIds"];
         }
     }
 }
