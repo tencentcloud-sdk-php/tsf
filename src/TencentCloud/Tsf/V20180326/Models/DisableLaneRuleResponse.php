f4d1fa77483fe43ab2a5d4440b7f096b0065ea2c
tencentcloudapi <tencentcloudapi@tencent.com>
2024-05-22 05:32:10 +0800

    release 3.0.1146

--- src/TencentCloud/Tsf/V20180326/Models/DisableLaneRuleResponse.php
+++ src/TencentCloud/Tsf/V20180326/Models/DisableLaneRuleResponse.php
@@ -18,7 +18,7 @@ namespace TencentCloud\Tsf\V20180326\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * ModifyLaneRule返回参数结构体
+ * DisableLaneRule返回参数结构体
  *
  * @method boolean getResult() 获取操作状态。成功：true，失败：false
 注意：此字段可能返回 null，表示取不到有效值。
@@ -27,7 +27,7 @@ use TencentCloud\Common\AbstractModel;
  * @method string getRequestId() 获取唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
  */
-class ModifyLaneRuleResponse extends AbstractModel
+class DisableLaneRuleResponse extends AbstractModel
 {
     /**
      * @var boolean 操作状态。成功：true，失败：false
